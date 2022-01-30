fdacd00883c7f858c3d65c7d365e677d1a2c9320
Oguz <yilmazoguz@outlook.com>
2022-01-31 00:20:22 +0300

    Add h2, p and img with the {image}

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div id="About">About</div>;
+  return (
+    <div id="About">
+      <h2>About Me</h2>
+      <p>I am 27.</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
